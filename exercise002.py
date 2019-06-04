dd777dce22feeae3c8b589f412420e7ee628f326
Rodrigo <rodrigoskw@gmail.com>
2019-06-04 09:06:31 -0300

    Primeiro exercicio

---
+++ exercise002.py
@@ -0,0 +1,13 @@
+
+resp = 42
+pi = 3.14159
+
+print(resp + pi)
+print(int(pi))
+print(float(resp))
+print("hello".capitalize())
+print("hello".replace("e","a"))
+print("hello".isalpha())
+print("hello".isdigit())
+print("Prazer em conhece-lo {0}. Eu sou {1}".format("Rodrigo", "Maraí"))
+
