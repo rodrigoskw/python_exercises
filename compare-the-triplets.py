8a31fe2db7fbba43655546e89a011f4b64f3141d
Rodrigo <rodrigoskw@gmail.com>
2019-06-07 18:00:44 -0300

    inclusao de novos exercicios

---
+++ compare-the-triplets.py
@@ -0,0 +1,9 @@
+def compareTriplets(a, b):
+    print(a)
+    print(b)
+
+a = list(map(int, input().rstrip().split()))
+
+b = list(map(int, input().rstrip().split()))
+
+result = compareTriplets(a, b)
