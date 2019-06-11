33c8982397e84f443568a805cc3b31242de3ee4f
Rodrigo <rodrigoskw@gmail.com>
2019-06-10 21:09:34 -0300

    Exercicio 038

---
+++ ex038.py
@@ -0,0 +1,10 @@
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o seundo número: '))
+
+if num1 > num2:
+    print('O primeiro número é maior !!!')
+elif num2 > num1:
+    print('O segundo número é maior !!!')
+else:
+    print('Os números são iguais !!!')
+
